0ecf008b03a98d039bf86d7b795ef1638b40a5d7
mmarty01 <mmarty01@cst-tp-b10114>
2019-12-06 15:48:20 +0100

    test

--- src/test/java/FFSSM/Test.java
+++ src/test/java/FFSSM/Test.java
@@ -6,6 +6,7 @@
 package FFSSM;
 
 import java.util.Calendar;
+import org.junit.Assert;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
@@ -17,11 +18,21 @@ import org.junit.Before;
 public class Test {
     Personne p;
 	Club c;
-	
+	Moniteur m;
+        Site s;
+        Plongeur plongeur;
+        Licence l;
+        
 	@Before
 	public void setUp() {
 		p = new Personne("1 61 03 34", "Bastide", "Rémi", null, null, null);
+                l= new Licence(p, "10", Calendar.getInstance(), 0, c);
+                plongeur= new Plongeur("1 61 03 34", "Bastide", "Rémi", null, null, null,l);
 		c = new Club(null, "Biarritz", null);
+                m=new Moniteur("154542", "Faucher", "Jonathan", null, null, null, 0);
+                s=new Site("Agout",null);
+                
+                
 	}
 	
 	@org.junit.Test
@@ -52,5 +63,26 @@ public class Test {
 		d2.add(Calendar.DAY_OF_YEAR, -1);
 		assertFalse( l.estValide(d2) );
 	}
+        @org.junit.Test
+        public void plongeeConforme(){
+            Plongee pl= new Plongee(s, m, Calendar.getInstance(), 250, 30);
+            pl.ajouteParticipant(plongeur);
+            assertTrue(pl.estConforme());
+            c.organisePlongee(pl);
+            Assert.assertEquals(0,c.plongeesNonConformes().size());
+            
+        }
+        @org.junit.Test
+        public void plongeeNonConforme(){
+            Calendar limiteDeValidite = Calendar.getInstance();
+        limiteDeValidite.add(Calendar.YEAR, 2);
+            Plongee pl= new Plongee(s, m, limiteDeValidite, 250, 30);
+            pl.ajouteParticipant(plongeur);
+            assertFalse(pl.estConforme());
+            c.organisePlongee(pl);
+            Assert.assertEquals(1,c.plongeesNonConformes().size());
+            
+        }
+        
 }
 
